c02a6b321a478553244f42ccfb4902eb45340cc7
Kent Simmons <kent1372@gmail.com>
2022-04-06 09:07:39 +1200

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+/* Excercise 8
+* 
+	Define an Order class with (customer) name, address, data, and
+	vector<Purchase> members. Purchase is a class with a (product) name,
+	unit_price, and count members. Define a mechanism for reading and
+	writing Orders to and from a file. Define a mechanism for printing Orders.
+	Create a file of at least ten Orders, read it into a vector<Order>, sort
+	it by name (of customer), and write it back out to a file. Create another
+	file of at least ten Orders of which about a third are the same as in the first
+	file, read it into a list<Order>, sort it by address (of customer), and write
+	it back out to a file. Merge the two files into a third using std::merge().
+* 
+*/
+
+int main()
+{
+	
+}
